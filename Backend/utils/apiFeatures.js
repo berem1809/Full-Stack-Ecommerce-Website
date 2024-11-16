9600df7941d0700e5b8f583ab241cf926127e2f2
Piranaavei Beremkumar <108848056+berem1809@users.noreply.github.com>
2024-11-16 13:51:42 +0530

    Complete the Paginate

--- Backend/utils/apiFeatures.js
+++ Backend/utils/apiFeatures.js
@@ -1,32 +1,41 @@
 class APIFeatures {
-    constructor(query, queryStr) {
+  constructor(query, queryString) {
       this.query = query;
-      this.queryStr = queryStr;
-    }
-  
-    search() {
-      let keyword = this.queryStr.keyword
-        ? {
-            name: {
-              $regex: this.queryStr.keyword,
-              $options: "i",
-            },
-          }
-        : {};
-  
-      this.query.find({ ...keyword });
-      return this;
-    }
-  
-    filter() {
-      const queryStrCopy = { ...this.queryStr };
-      const removeField = ["keyword", "limit", "page"];
-  
-      removeField.forEach((field) => delete queryStrCopy[field]);
-  
-      this.query.find(queryStrCopy);
-      return this;
-    }
+      this.queryString = queryString;
   }
-  
-  module.exports = APIFeatures;
+
+  search() {
+      const keyword = this.queryString.keyword ? {
+          name: {
+              $regex: this.queryString.keyword,
+              $options: 'i'
+          }
+      } : {}
+
+      this.query = this.query.find({ ...keyword });
+      return this;
+  }
+
+  filter(){
+      const queryCopy = { ...this.queryString };
+
+      // Removing fields from the query
+      const removeFields = ['keyword', 'limit', 'page']
+      removeFields.forEach(field => delete queryCopy[field]);
+
+      // Advance filter for price, ratings etc
+      let queryStr = JSON.stringify(queryCopy)
+      queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+      this.query = this.query.find(JSON.parse(queryStr));
+      return this;
+  }
+  paginate(resPerPage){
+      const currentPage = Number (this.queryStr.page ) || 1;
+      const skip = resPerPage * (currentPage - 1)
+      this.query.limit(resPerPage).skip(skip);
+      return this;
+
+  }
+}
+module.exports = APIFeatures;
