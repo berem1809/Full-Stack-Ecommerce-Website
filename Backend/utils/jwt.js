24c98fb0dee29a2e7dc909806526f660da4f7bb8
Piranaavei Beremkumar <108848056+berem1809@users.noreply.github.com>
2024-11-18 20:41:03 +0530

    Sending Token in Cookie

--- Backend/utils/jwt.js
+++ Backend/utils/jwt.js
@@ -1,13 +1,20 @@
 const sendToken = (user, statusCode, res) => {
     // Create JWT token
     const token = user.getJWTToken();
-
-    res.status(statusCode).json({
-        success: true,
-        token,
-        user
-    })
-    
-
-}
-module.exports = sendToken;
+  
+    //setting the cookie
+    const options = {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    };
+  
+    res.status(statusCode).cookie("token", token, options).json({
+      success: true,
+      token,
+      user,
+    });
+  };
+  module.exports = sendToken;
+  
