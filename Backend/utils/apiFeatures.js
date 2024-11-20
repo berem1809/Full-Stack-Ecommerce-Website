26822588d4701815af2013704074c28091f47754
Piranaavei Beremkumar <108848056+berem1809@users.noreply.github.com>
2024-11-20 23:44:12 +0530

    Some Modifications on apiFeatures.js

--- Backend/utils/apiFeatures.js
+++ Backend/utils/apiFeatures.js
@@ -1,41 +1,85 @@
+// class APIFeatures {
+//   constructor(query, queryString) {
+//       this.query = query;
+//       this.queryString = queryString;
+//   }
+
+//   search() {
+//       const keyword = this.queryString.keyword ? {
+//           name: {
+//               $regex: this.queryString.keyword,
+//               $options: 'i'
+//           }
+//       } : {}
+
+//       this.query = this.query.find({ ...keyword });
+//       return this;
+//   }
+
+//   filter(){
+//       const queryCopy = { ...this.queryString };
+
+//       // Removing fields from the query
+//       const removeFields = ['keyword', 'limit', 'page']
+//       removeFields.forEach(field => delete queryCopy[field]);
+
+//       // Advance filter for price, ratings etc
+//       let queryStr = JSON.stringify(queryCopy)
+//       queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+//       this.query = this.query.find(JSON.parse(queryStr));
+//       return this;
+//   }
+//   paginate(resPerPage){
+//       const currentPage = Number (this.queryStr.page ) || 1;
+//       const skip = resPerPage * (currentPage - 1)
+//       this.query.limit(resPerPage).skip(skip);
+//       return this;
+
+//   }
+// }
+// module.exports = APIFeatures;
+
 class APIFeatures {
-  constructor(query, queryString) {
-      this.query = query;
-      this.queryString = queryString;
-  }
+    constructor(query, queryStr) {
+        this.query = query;
+        this.queryStr = queryStr;
+    }
 
-  search() {
-      const keyword = this.queryString.keyword ? {
-          name: {
-              $regex: this.queryString.keyword,
-              $options: 'i'
-          }
-      } : {}
+    search() {
+        const keyword = this.queryStr.keyword ? {
+            name: {
+                $regex: this.queryStr.keyword,
+                $options: 'i'
+            }
+        } : {};
 
-      this.query = this.query.find({ ...keyword });
-      return this;
-  }
+        this.query = this.query.find({ ...keyword });
+        return this;
+    }
 
-  filter(){
-      const queryCopy = { ...this.queryString };
+    filter() {
+        const queryCopy = { ...this.queryStr };
 
-      // Removing fields from the query
-      const removeFields = ['keyword', 'limit', 'page']
-      removeFields.forEach(field => delete queryCopy[field]);
+        // Removing fields from the query
+        const removeFields = ['keyword', 'page', 'limit'];
+        removeFields.forEach(el => delete queryCopy[el]);
 
-      // Advance filter for price, ratings etc
-      let queryStr = JSON.stringify(queryCopy)
-      queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+        // Advanced filter for price, ratings etc
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
 
-      this.query = this.query.find(JSON.parse(queryStr));
-      return this;
-  }
-  paginate(resPerPage){
-      const currentPage = Number (this.queryStr.page ) || 1;
-      const skip = resPerPage * (currentPage - 1)
-      this.query.limit(resPerPage).skip(skip);
-      return this;
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
 
-  }
+    paginate(resultsPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+        const skip = resultsPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resultsPerPage).skip(skip);
+        return this;
+    }
 }
+
 module.exports = APIFeatures;
