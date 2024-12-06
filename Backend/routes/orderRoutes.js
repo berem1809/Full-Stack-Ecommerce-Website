7249f6f4a5cd66c3e920efba170b6bed4741740f
Piranaavei Beremkumar <108848056+berem1809@users.noreply.github.com>
2024-12-06 08:46:10 +0530

    Delete the an order by Admin.

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { newOrder ,  getSingleOrder,myOrders , getAllOrders ,updateOrder } = require("../controllers/orderController");
+const { newOrder ,  getSingleOrder,myOrders , getAllOrders ,updateOrder,deleteOrder } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/authenticate");
 
 router.route('/order/new').post(isAuthenticatedUser,newOrder);
@@ -10,6 +10,7 @@ router.route('/myorders').get(isAuthenticatedUser,myOrders);
 //Admin Routes
 router.route('/admin/orders').get(isAuthenticatedUser,authorizeRoles('admin'),getAllOrders);  
 router.route('/admin/order/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateOrder);
+router.route('/admin/order/:id').delete(isAuthenticatedUser,authorizeRoles('admin'),deleteOrder);
 
 
 module.exports = router;
